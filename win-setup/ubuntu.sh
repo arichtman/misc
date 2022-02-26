0e2f116fcee6d91c5694372b934160df5ceac04a
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2022-02-27 09:45:40 +1000

    add poetry config

--- win-setup/ubuntu.sh
+++ win-setup/ubuntu.sh
@@ -2,3 +2,11 @@
 
 sudo cp podman /etc/init.d/
 sudo chmod +x /etc/init.d/podman
+
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get install python
+
+curl -sSL https://install.python-poetry.org | python3 -
+
+poetry config virtualenvs.in-project true
