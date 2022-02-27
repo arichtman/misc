768e28b9bab82b872bf70579a0c0bc6c35d3322f
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2022-02-27 11:05:31 +1000

    add pre-commit global install and required python development packages

--- win-setup/ubuntu.sh
+++ win-setup/ubuntu.sh
@@ -5,8 +5,12 @@ sudo chmod +x /etc/init.d/podman
 
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
-sudo apt-get install python
+sudo apt-get install python3.11 python3.11-distutils python3.11-dev
 
 curl -sSL https://install.python-poetry.org | python3 -
 
 poetry config virtualenvs.in-project true
+
+
+git config --global init.templateDir ~/.git-template
+pre-commit init-templatedir ~/.git-template
